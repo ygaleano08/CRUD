b4be8549b3236fc1a69dfa42082b8df4b785b16d
unknown <ygaleano08@gmail.com>
2020-10-05 23:45:16 -0600

    cambios en el crud

--- crud.js
+++ crud.js
@@ -1,5 +1,4 @@
 import cars from './cars.js';
-
 // function printCars() {
 //   for(let i = 0; i < cars.length; i++) {
 //       const row = '<tr><td>'+cars[i].brand+'</td><td>' + cars[i].model + '</td></tr>'
@@ -31,14 +30,17 @@ function printCars() {
 // ESTA FUNCION RECIBE EL DATO DE QUE FILA FUE SELECCIONADA EN EL HTML Y CARGA LOS DATOS EN LOS INPUTS
 function loadData(indexArray) {
   let wichSelect = indexArray;
+  idInput.value = cars[wichSelect].id
   brandInput.value = cars[wichSelect].brand
   modelInput.value = cars[wichSelect].model
   colorInput.value = cars[wichSelect].color
   yearInput.value = cars[wichSelect].year
-  priceInput.value = cars[wichSelect].price
-  return wichSelect;
+  priceInput.value = cars[wichSelect].price 
+  // return wichSelect()
+  // alert(wichSelect)
 }
 
+// ESTA FUNCION ARMA UN NUEVO OBJETO DESDE LOS VALORES DE LOS INPUTS Y LE HACE PUSH AL ARREGLO DE DATOS
 function addData () {
    cars.push({
     id:cars.length+1,
@@ -47,15 +49,28 @@ function addData () {
     color: colorInput.value,
     year: yearInput.value,
     price:priceInput.value
-  })
+  });
 printCars()
 }
 
-
-
+// ESTA FUNCION ARMA UN NUEVO OBJETO DESDE LOS VALORES DE LOS INPUTS Y LE HACE SPLICE AL ARREGLO DE DATOS para editarlo
+function editData () {
+  const position = idInput.value;
+  let nvalor = {
+    id: 1, 
+    brand: brandInput.value, 
+    model: modelInput.value, 
+    color: colorInput.value, 
+    year: yearInput.value, 
+    price: priceInput.value
+  }
+  cars.splice (position, 1, nvalor)
+printCars();
+}
 
 printCars();
 
 window.printCars = printCars;
 window.loadData = loadData;
 window.addData = addData;
+window.editData = editData;
