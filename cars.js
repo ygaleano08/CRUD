7b354c0596651ad098429ec21e0740a9779c3aae
unknown <ygaleano08@gmail.com>
2020-10-05 22:22:47 -0600

    Comit antes de subir

---
+++ cars.js
@@ -0,0 +1,44 @@
+const cars = [
+  {
+    id: 1,
+    brand: 'Toyota',
+    model: 'Yaris',
+    color: 'Crema',
+    year: '2006',
+    price: 6000
+  },
+  {
+    id: 2,
+    brand: 'Honda',
+    model: 'crv',
+    color: 'plata',
+    year: '2015',
+    price: 15000
+  },  
+  {
+    id: 3,
+    brand: 'Hyundai',
+    model: 'Accent',
+    color: 'Rojo',
+    year: '2010',
+    price: 12000
+  },  
+  {
+    id: 4,
+    brand: 'Susuki',
+    model: 'Jimny',
+    color: 'Verde',
+    year: '2008',
+    price: 9000
+  },  
+  {
+    id: 5,
+    brand: 'Tesla',
+    model: 'S',
+    color: 'Gris',
+    year: '2015',
+    price: 75000
+  },
+];
+
+export default cars;
---
+++ crud.js
@@ -0,0 +1,61 @@
+import cars from './cars.js';
+
+// function printCars() {
+//   for(let i = 0; i < cars.length; i++) {
+//       const row = '<tr><td>'+cars[i].brand+'</td><td>' + cars[i].model + '</td></tr>'
+//       mainTbody.innerHTML += row;
+//   }
+// }
+
+
+// al inicio de esta funcion SE ESCRIBE EL ID EN CADA FILA DE LA TABLA PARA LUEGO SER PASADO COMO PARAMETRO CUANDO SE DE CLICK y se active la funcion loadData
+// Que lleva consigo el id del array que se selecciono
+
+function printCars() {
+  mainTbody.innerHTML = ''
+  for(let i = 0; i < cars.length; i++) {
+      const row = `<tr id = "${cars[i].id}" onClick = "loadData(${i})">
+                      <td>${cars[i].id}</td>
+                      <td>${cars[i].brand}</td>
+                      <td>${cars[i].model}</td>
+                      <td>${cars[i].color}</td>
+                      <td>${cars[i].year}</td>
+                      <td> $ ${cars[i].price}</td>
+                      <td> <button onclick="NOMBREFUNCION" class="btn btn-primary"> Seleccionar </button> </td>
+                  </tr>`
+      mainTbody.innerHTML += row;
+  }
+}
+
+
+// ESTA FUNCION RECIBE EL DATO DE QUE FILA FUE SELECCIONADA EN EL HTML Y CARGA LOS DATOS EN LOS INPUTS
+function loadData(indexArray) {
+  let wichSelect = indexArray;
+  brandInput.value = cars[wichSelect].brand
+  modelInput.value = cars[wichSelect].model
+  colorInput.value = cars[wichSelect].color
+  yearInput.value = cars[wichSelect].year
+  priceInput.value = cars[wichSelect].price
+  return wichSelect;
+}
+
+function addData () {
+   cars.push({
+    id:cars.length+1,
+    brand: brandInput.value,
+    model: modelInput.value,
+    color: colorInput.value,
+    year: yearInput.value,
+    price:priceInput.value
+  })
+printCars()
+}
+
+
+
+
+printCars();
+
+window.printCars = printCars;
+window.loadData = loadData;
+window.addData = addData;
